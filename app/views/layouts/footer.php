4c9af9b08cf988244ad74409e1c6bf61aa164d6c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-04-11 02:39:44 -0600

    Se agrego el script del navbar para el admin

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,6 +1,7 @@
 <script src="<?php echo asset("js", "jquery-3.7.1.min.js") ?>"></script>
 <script type="module" src="<?php echo asset("js", "functions.js") ?>"></script>
 <script type="module" src="<?php echo asset("js", "login.js") ?>"></script>
+<script type="module" src="<?php echo asset("js", "navbar-admin.js") ?>"></script>
 
 </body>
 
