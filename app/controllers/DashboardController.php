2c0c47b9edca159ac7f62139f4c2313d7851655c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-04-11 17:37:16 -0600

    Se agrego la obtención de la sesión del usuario y se le paso a la vista del Dashboard

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -2,9 +2,11 @@
 
 class DashboardController
 {
+    private $user;
 
     public function index()
     {
-        loadView("dashboard", "index", [], true, true);
+        $this->user = getSession("user");
+        loadView("dashboard", "index", ["user" => $this->user], true, true);
     }
 }
