d4fc591a0a07d846a0158db39273094203b51370
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-04-12 11:59:36 -0600

    Se agrego el modelo de las Categorias

---
+++ app/models/Categoria.php
@@ -0,0 +1,65 @@
+<?php
+
+
+class Categoria
+{
+
+    private $connection;
+
+    public function __construct($conn)
+    {
+        $this->connection = $conn;
+    }
+
+    public function add($nombre, $descripcion, $imagen, $id_padre = null)
+    {
+        $query = "INSERT INTO categoria (nombre, descripcion, imagen,  id_categoria_padre) VALUES (?, ?, ?, ?)";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("sssi", $nombre, $descripcion, $imagen, $id_padre);
+        $stmt->execute();
+        return $stmt->affected_rows > 0;
+        $stmt->close();
+    }
+
+    public function getAll()
+    {
+        $query = "SELECT * FROM categoria WHERE id_categoria_padre IS NULL";
+        $stmt = $this->connection->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function delete($id)
+    {
+        $query = "DELETE FROM categoria WHERE id = ?";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        return $stmt->affected_rows > 0;
+        $stmt->close();
+    }
+
+    public function getById($id)
+    {
+        $query = "SELECT * FROM categoria WHERE id = ?";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        return $result->fetch_assoc();
+    }
+
+    public function getByName($name)
+    {
+        $query = "SELECT * FROM categoria WHERE nombre = ?";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("s", $name);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        return $result->fetch_assoc();
+    }
+}
